5ea6eb3a6fcbf2d3c9af37c4423601703138aeff
M Bilal <bilalqurehi.epic@gmail.com>
2017-08-16 00:24:24 +0500

    Gson Working

---
+++ app/src/main/java/com/example/mhassan/gson/models/Student.java
@@ -0,0 +1,17 @@
+package com.example.mhassan.gson.models;
+
+/**
+ * Created by M.Hassan on 8/1/2017.
+ */
+
+/*public class Student {
+}*/
+
+public class Student {
+
+    int id;
+    String first_name;
+    boolean gender;
+    String imageurl;
+}
+
